6b0a27f10c870eabeac1453fde4efb06ba8c2469
Samuel Cornejo <sam.ach1990@gmail.com>
2020-03-10 02:20:17 -0600

    [FlowRate] modifies the current implementation to calculate flow rate in order to use a safer one

--- Sources/SwiftFlowMeter/SwiftFlowMeter.swift
+++ Sources/SwiftFlowMeter/SwiftFlowMeter.swift
@@ -14,59 +14,77 @@
 import Foundation
 import SwiftyGPIO
 
-public enum MonitoringAction {
-    case start
-    case stop
-}
-
 public class SwiftFlowMeter {
+    public enum State {
+        case idle
+        case available
+        case queuedWork
+    }
+    
     // MARK: Variables Declaration
     private let gpio: GPIO
+    private let sensorQueue: DispatchQueue
     private let pulseCharacteristic: SensorCharacteristic
-    private var currentAction: MonitoringAction = .stop
-    private var pulseCount: Double = 0
+
+    private var flowRateCalculationWorkItem: DispatchWorkItem?
+    private var onFlowRateCalculation: ((FlowRate) -> Void)?
+    private var pulseCount: Double
+    private var currentFlowRate: FlowRate
+    private var flowRateHistory: [FlowRate]
+    
+    private var state: SwiftFlowMeter.State {
+        didSet {
+            if state == .available {
+                onFlowRateCalculation?(currentFlowRate)
+                readFlowRate(onCalculation: onFlowRateCalculation)
+            }
+        }
+    }
     
     // MARK: Initializer
-    public init(for board: SupportedBoard = .RaspberryPi3, pinName: GPIOName, pulseCharacteristic: SensorCharacteristic) {
+    public init(for board: SupportedBoard = .RaspberryPi3,
+                pinName: GPIOName,
+                queue: DispatchQueue = DispatchQueue(label: "com.swiftflowmeter.queue"),
+                pulseCharacteristic: SensorCharacteristic) {
         let gpios = SwiftyGPIO.GPIOs(for: board)
         
-        guard gpios.contains(where: { $0.key == pinName }) else {
+        guard gpios.contains(where: { $0.key == pinName }), let gpioPin = gpios[pinName] else {
             fatalError("Please make sure the selected pin is available on the selected board.")
         }
         
-        self.gpio = gpios[pinName]!
+        self.gpio = gpioPin
+        self.sensorQueue = queue
         self.pulseCharacteristic = pulseCharacteristic
+        self.pulseCount = 0
+        self.currentFlowRate = FlowRate(pulses: pulseCount, using: pulseCharacteristic)
+        self.flowRateHistory = []
+        self.state = .idle
         
         configurePin()
     }
     
     // MARK: Public Methods
     
-    /// Changes the sensor's monitoring action.
-    /// - Parameter newAction: The new action to be executed
-    public func switchMonitoring(to newAction: MonitoringAction) {
-        currentAction = newAction
-    }
-    
     /// Reads the sensor's current flow rate and reports it back every second.
-    /// - Parameters:
-    ///   - queue: The queue where the operation is going to take place
-    ///   - onCalculation: The closure to be executed every second the flow rate is calculated
-    public func readFlowRate(queue: DispatchQueue = DispatchQueue(label: "com.swiftflowmeter.readflowrate"), onCalculation: @escaping (FlowRate) -> Void) {
-        currentAction = .start
-        pulseCount = 0
+    /// - Parameter onCalculation: The closure to be executed every second the flow rate is calculated
+    public func readFlowRate(onCalculation: ((FlowRate) -> Void)? = nil) {
+        self.onFlowRateCalculation = onCalculation
         
-        queue.async { [weak self] in
+        flowRateCalculationWorkItem = DispatchWorkItem { [weak self] in
             guard let self = self else { return }
             
-            var startTime = Date()
-            while self.currentAction == .start {
-                if abs(startTime.timeIntervalSinceNow) > 1 {
-                    onCalculation(FlowRate(pulses: self.pulseCount, using: self.pulseCharacteristic))
-                    
-                    self.pulseCount = 0
-                    startTime = Date()
-                }
+            self.currentFlowRate = FlowRate(pulses: self.pulseCount, using: self.pulseCharacteristic)
+            self.pulseCount = 0
+            self.state = .available
+        }
+        
+        sensorQueue.asyncAfter(deadline: .now() + .seconds(1), execute: flowRateCalculationWorkItem!)
+        state = .queuedWork
+    }
+    
+    /// - Parameters:
+            guard let self = self else { return }
+            
             }
         }
     }
