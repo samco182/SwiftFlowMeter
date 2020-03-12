2fce042b25624021bcc194fd011ef92688ef9d8d
Samuel Cornejo <sam.ach1990@gmail.com>
2020-03-12 02:12:48 -0600

    [Example] adds example project code

--- Example/Package.swift
+++ Example/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SwiftFlowMeterExample",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(path: "./../")
+        .package(url: "https://github.com/samco182/SwiftFlowMeter", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Example/README.md
+++ Example/README.md
@@ -1,3 +1,3 @@
 # SwiftFlowMeterExample
 
-A description of this package.
+A simple example of how to get readings from a Hall effect based water flow sensor using SwiftFlowMeter library.
