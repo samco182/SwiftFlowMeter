9e85ff0133adc71b9f96aaa4e63f74c0649c401a
Samuel Cornejo <sam.ach1990@gmail.com>
2020-03-12 03:20:03 -0600

    [Update] updates SwiftyGPIO dependency to use an updated one

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.0.0"),
+        .package(url: "https://github.com/samco182/SwiftyGPIO.git", .branch("next_release")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
